ce85ee75075e9cfdb48a301ea45ef9d1c2e916d7
Nanqin0 <140690953+Nanqin0@users.noreply.github.com>
2023-11-12 18:37:00 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,11 @@
 import streamlit as st
+import boto3
 from utilities.AI_API import *
 import PyPDF2
 import os
 import re
 
-# Ensure there's a folder to save uploaded files
+#Ensure there's a folder to save uploaded files
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -17,17 +18,26 @@ def convert_pdf_to_text(file_path):
             text += reader.getPage(page).extractText()
         return text
 
-def generate_summary(resume_text):
-    # Placeholder function for generating a summary
-    # Implement more complex logic as needed
-    return "This is a placeholder summary."
-
-def identify_strengths_weaknesses(resume_text):
-    # Placeholder function for identifying strengths and weaknesses
-    # Implement more complex logic as needed
-    strengths = "Placeholder strengths"
-    weaknesses = "Placeholder weaknesses"
-    return strengths, weaknesses
+# Function to upload a file to an S3 bucket
+def upload_to_s3(file_name, bucket_name):
+    s3_client = boto3.client('s3')
+    try:
+        s3_client.upload_file(file_name, bucket_name, file_name)
+        print("Upload Successful")
+        return True
+    except FileNotFoundError:
+        print("The file was not found")
+        return False
+    except NoCredentialsError:
+        print("Credentials not available")
+        return False
+        
+def analyze_resume(resume_text):
+    # Use the functions from your AI API
+    summary = generate_summary(resume_text)
+    strengths = identify_strengths(resume_text)
+    weaknesses = identify_weaknesses(resume_text)
+    return summary, strengths, weaknesses
 
 def extract_skills(resume_text):
     # Define a list of skills to look for
@@ -40,6 +50,9 @@ def extract_skills(resume_text):
             found_skills.add(skill)
     return list(found_skills)
 
+# Streamlit UI
+st.header("Resume Analysis Tool")
+
 with st.sidebar:
     st.header("Upload Your Resume")
     uploaded_file = st.file_uploader("Choose a file", type='pdf')
@@ -47,34 +60,21 @@ with st.sidebar:
         file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
         with open(file_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
-        
+
+        # Upload file to S3
+        if upload_to_s3(file_path, 'your-s3-bucket-name'):
+            st.success('File uploaded to S3 successfully')
+
         # Convert the uploaded PDF to text
         resume_text = convert_pdf_to_text(file_path)
-        st.success('File uploaded successfully')
 
-        # Analyze the resume
-        summary = generate_summary(resume_text)
-        strengths, weaknesses = identify_strengths_weaknesses(resume_text)
-        skills = extract_skills(resume_text)
+        # Analyze the resume using your AI API
+        summary, strengths, weaknesses = analyze_resume(resume_text)
 
         # Display the analysis results
         st.subheader("Resume Analysis")
         st.text_area("Summary", summary, height=100)
         st.text_area("Strengths", strengths, height=100)
         st.text_area("Weaknesses", weaknesses, height=100)
-        st.multiselect("Skills Extracted", skills, default=skills)
-
-# Add a section in your Streamlit UI for resume upload
-with st.sidebar:
-    st.header("Upload Your Resume")
-    uploaded_file = st.file_uploader("Choose a file", type='pdf')
-    if uploaded_file is not None:
-        file_path = os.path.join(UPLOAD_FOLDER, uploaded_file.name)
-        with open(file_path, "wb") as f:
-            f.write(uploaded_file.getbuffer())
-        # Convert the uploaded PDF to text
-        resume_text = convert_pdf_to_text(file_path)
-        st.success('File uploaded successfully')
-        st.text_area("Extracted Text", resume_text, height=300)
 
 
